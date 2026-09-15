9047e71a297beaba87249d1b3ff24efbe719c340
junim887876 <junimbarberofc@gmail.com>
2026-09-14 21:10:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
+const CACHE_NAME = 'solarinvest-v1';
 
+// Instalação do Service Worker
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+// Ativação do Service Worker
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+// Interceptação de requisições
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
